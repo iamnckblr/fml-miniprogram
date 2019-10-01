9beb0fe189ca17f3c282caec45f1051bb0396c01
Nico_surface <nico.kobler@gmx.ch>
2019-10-01 08:44:18 +0200

    working version

---
+++ models/fml.js
@@ -0,0 +1,9 @@
+// model/form.js
+const AV = require('../utils/av-weapp-min.js');
+class FML extends AV.Object {
+
+}
+// Register object
+AV.Object.register(FML, 'FML');
+
+module.exports = FML;
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,35 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const AV = require('../../utils/av-weapp-min.js');
+const FML = require('../../models/fml.js');
+
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    stories: [],
+    updated: false
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+    new AV.Query('FML')
+      .descending('createdAt')
+      .find()
+      .then(fmls => {
+        this.setData({ stories: fmls });
+        console.log(fmls)
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+      .catch(console.error);
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  upvoteFML: function(e) {
+    var selectedIndex = this.data.stories.findIndex(FML => FML.id === e.target.id)
+    this.data.stories[selectedIndex].increment("likes", 1).save().then(fml => {
+      new AV.Query('FML')
+        .descending('createdAt')
+        .find()
+        .then(fmls => {
+          this.setData({ stories: fmls });
+          console.log(fmls)
+        })
+        .catch(console.error);
+    });
   }
 })
